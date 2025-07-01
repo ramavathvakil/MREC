74acbc032cdad90dcb6e93303560b9322c5b1c0d
ramavathvakil <rathodvakeel59@gmail.com>
2025-07-01 10:51:38 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,38 @@
+"""a=[1,6,6,8,8,1,2,1,4,5,6,7,7,8,"vamshi","vakil",0.99]
+s=set(a)
+print(s)"""
+# if 0.99 in s:
+#     print("element is present")
+# else:
+#     print("not present")
+"""count=0
+for i in s:
+    count=count+1
+print(count)"""
+# for i in range(2,10):
+#     print(i,end=" ")
+
+
+# target=6
+# for i in range(n):
+#     if a[i]==target:
+#         print("true")
+#         break
+# for i in a:
+#     if a[i]==target:
+#         print(True)
+#         break
+#     else:
+#         print(False)
+
+
+# a=[1,12,3,4,5,3,6,7,8,9,7,5,2]
+# n=len(a)
+# target=7
+a=[1,12,3,4,5,3,6,7,8,9,7,5,2]
+maxi=a[0]
+for i in a:
+    if i>maxi:
+        maxi=i
+print(maxi)       
+    
